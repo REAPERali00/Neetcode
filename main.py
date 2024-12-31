72edbb5033ab6637404988ea53741d0d48f696a5
Alireza <alireza2000jadidi@gmail.com>
2024-12-30 19:24:45 -0500

    solved cpu heap proble

--- main.py
+++ main.py
@@ -390,6 +390,31 @@ class Solution:
         while len(nums) > k: 
             heapq.heappop(nums)
         return nums[0]
+    # TODO: This is a great example of que, map and heap
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()  # pairs of [-cnt, idleTime]
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time 
+
+            
+
+
+
 
 
 sol = Solution()
