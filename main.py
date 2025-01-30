1a74ff9770a42447d083c2783ef60d6be7cc96fa
alireza <alireza2000jadidi@gmail.com>
2025-01-30 18:03:08 -0500

    added greedy

--- main.py
+++ main.py
@@ -605,4 +605,15 @@ class Solution:
 
         return res
         
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost): return -1
+        res = tot = 0 
+        for i in range(len(gas)): 
+            tot += gas[i]- cost[i]
+            if tot < 0:
+                tot =0 
+                res = i+1
+        return res
+
+
 
